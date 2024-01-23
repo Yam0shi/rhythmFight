df594e41bce514b01f8b78d6fa1f8a9c765cc1da
Yam0shi <tessiera00@gmail.com>
2024-01-24 00:54:28 +0100

    startScript
    début ennemy V1
    controle du joueur à l'arrache V1

--- Assets/Script/BossScript.cs
+++ Assets/Script/BossScript.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform[] ennemySpawner;
+    [SerializeField] private List<Transform> AllBullets = new();
+    [SerializeField] private GameObject ennemyPrefab;
+    [SerializeField] private float bulletSpeed;
+
     void Start()
     {
-        
+        StartCoroutine(EnnemySpawner());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        EnnemyBulletMoving();
+    }
+
+    void EnnemyBulletMoving()
+    {
+        for (int i = 0; i < AllBullets.Count; i++)
+        {
+            AllBullets[i].transform.position += Vector3.back * bulletSpeed * Time.deltaTime;
+        }
+    }
+
+    private IEnumerator EnnemySpawner()
+    {
+        int indexRandom = Random.Range(0, ennemySpawner.Length);
+        GameObject ennemyBullet = Instantiate(ennemyPrefab, ennemySpawner[indexRandom].position, Quaternion.identity, ennemySpawner[indexRandom]);
+        AllBullets.Add(ennemyBullet.transform);
+        yield return new WaitForSeconds(3);
+        if(ennemyBullet.transform.position.z <= 3)
+            Destroy(ennemyBullet);
+
+        StartCoroutine(EnnemySpawner());
     }
 }
--- Assets/Script/PlayerControls.cs
+++ Assets/Script/PlayerControls.cs
@@ -3,9 +3,11 @@ using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour
 {
+    const float AxisY = .6f;
+
     [Header("Stats :")]
     [SerializeField] private int playerLife;
-    [SerializeField] private float playerSpeed;
+    [SerializeField] private float playerSpeed = 6, jumpSpeed = 8;
 
     [Header("Script")]
     [SerializeField] private GameManager theManager;
@@ -17,9 +19,16 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] private Slider sliderLife;
     [SerializeField] private GameObject life;
 
+    [Header("other :")]
+    [SerializeField] private Vector3 initialPosWhenGoNextTo;
+    [SerializeField] private Transform[] waysForPlayer;
+    [SerializeField] private int indexOfWays;
+
     void Start()
     {
         theManager = gameObject.AddComponent<GameManager>();
+
+        indexOfWays = 2;
 
         sliderLife.maxValue = playerLife;
         sliderLife.value = playerLife;
@@ -39,6 +48,42 @@ public class PlayerControls : MonoBehaviour
             life.SetActive(true);
             playerLife--;
             sliderLife.value--;
+        }
+
+        MovingPlayer();
+    }
+
+    void MovingPlayer()
+    {
+        int nextIndex = indexOfWays;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            
+
+            if (nextIndex <= waysForPlayer.Length - 1)
+            {
+                nextIndex = indexOfWays + 1;
+            }else
+            nextIndex = 4;
+
+            transform.position = new Vector3(waysForPlayer[nextIndex].position.x, AxisY);
+
+            indexOfWays = nextIndex;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            if (nextIndex >= 0)
+            {
+                nextIndex = indexOfWays - 1;
+
+            }else
+            nextIndex = 0;
+
+            transform.position = new Vector3(waysForPlayer[nextIndex].position.x, AxisY);
+
+            indexOfWays = nextIndex;
         }
     }
 
